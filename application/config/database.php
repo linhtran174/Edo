ddec293dbdc0725b2292850e75bd98daccd8cb9e
hieunq <HieuNQ@ows.edu.vn>
2016-07-17 23:07:10 +0700

    config database va tao core model

--- application/config/database.php
+++ application/config/database.php
@@ -64,10 +64,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '115.84.179.142',
+	'username' => 'hoangnm_daotao',
+	'password' => 'daotao1234',
+	'database' => 'hoangnm_daotao',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
