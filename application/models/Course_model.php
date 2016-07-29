45ec397026f47115cfd16fb3e50ee1a680910740
hieunq <HieuNQ@ows.edu.vn>
2016-07-29 21:03:29 +0700

    fix ten ham

--- application/models/Course_model.php
+++ application/models/Course_model.php
@@ -3,15 +3,8 @@ class course_model extends MY_Model{
 	public $table = 'tblCourse';
 	public $table_id = 'course_id';
 
-	public function getCoursePage($page){
-		
-		$query = "SELECT * FROM ".$this->table." LIMIT ".($page*5).",".($page*5+5);
-		//SELECT * FROM tbtCourse LIMIT 10, 20
-		$result = $this->db->query($query)->result();
-		return $result;
-	}
 
-	public function filterCoure($input){
+	public function filter_coure($input){
 		if($input['level'] == null){
 			if($input['fee'] != 0){
 				$query = "SELECT * FROM ".$this->table." WHERE course_fee > 0";
