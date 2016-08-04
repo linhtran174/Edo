54974affbf9b55992cb6aeb18191e84a28004271
truongnm <truongnm2@ows.edu.vn>
2016-08-04 16:27:36 +0700

    update my classroom classroom_lessonComp

--- application/controllers/My_classroom.php
+++ application/controllers/My_classroom.php
@@ -38,17 +38,18 @@ class My_classroom extends CI_Controller{
 		//print_r($this->session->userdata("login"));
 		$student = $this->student_model->get_info_rule(array(
 			"stud_id" => $this->session->userdata("login")->stud_id
-		));
+			));
 		
 		$this->load->view('setting', array(
 			"student" => $student
-		));
+			));
 	}
 
 	public function load_lesson($course_id = 1, $lesson_id = 0){
 		if(!$this->session->userdata('login')){
 			redirect('/login', 'refresh');
 		}
+		$stud_id = $this->session->userdata('login')->stud_id;
 
 		$course = $this->course_model->get_course_detail($course_id);
 		if(!$course){
@@ -72,6 +73,43 @@ class My_classroom extends CI_Controller{
 			"active_lesson" => $active_lesson,
 			"active_course" => $active_course)
 		);
+
+		// get classroom detail and course progress
+		$input['where'] = array(
+			'class_course'=>$course_id,
+			'class_stud'=>$stud_id);
+		$classroom = $this->classroom_model->get_list($input);
+		if(!$classroom) {
+			$tmp = array(
+				// 'class_id'=>$course_id,
+				'class_stud'=>$stud_id,
+				'class_course'=>$course_id,
+				'class_comp'=>"",
+				'class_joinedAt'=>gmdate('Y-m-d', time()),
+				'class_lastAccess'=>gmdate('Y-m-d', time()),
+				'class_lessonComp'=>$topics[0]->lessons[0]->lesson_id. " ",
+				'class_lesson'=>$topics[0]->lessons[0]->lesson_id
+				);
+			$this->classroom_model->create($tmp);
+		} else {
+			if($lesson_id>0){
+				$classroom[0]->class_lesson = $lesson_id;
+
+				if(strpos($classroom[0]->class_lessonComp , $lesson_id) === false){
+					// echo "new lesson learning";
+					$tmp = $classroom[0]->class_lessonComp . $lesson_id . " ";
+					$classroom[0]->class_lessonComp = $tmp;
+					
+					
+				}
+			} else {
+				$classroom[0]->class_lesson = $topics[0]->lessons[0]->lesson_id;
+			}
+			$input = array(
+						'class_id'=>$classroom[0]->class_id
+						);
+			$this->classroom_model->update_rule($input, $classroom[0]);			
+		}
 
 		$this->load->view('studying',array(
 			"course" => $active_course,
